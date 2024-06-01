e62c4c7b155091c314b63ec3064a2b17cbbb9506
AlmostAnEngineer <94685050+AlmostAnEngineer@users.noreply.github.com>
2024-06-01 16:54:53 +0200

    [IMPRV] created aspect package in root

--- src/main/java/com/capgemini/wsb/fitnesstracker/aspect/LoggingAspect.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/aspect/LoggingAspect.java
@@ -1,4 +1,4 @@
-package com.capgemini.wsb.fitnesstracker;
+package com.capgemini.wsb.fitnesstracker.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;

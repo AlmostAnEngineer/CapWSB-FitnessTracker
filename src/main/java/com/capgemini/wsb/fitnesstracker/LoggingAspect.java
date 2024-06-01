00a9a6a6b892d8453c7840cb9d7524568a8fb3cd
AlmostAnEngineer <94685050+AlmostAnEngineer@users.noreply.github.com>
2024-06-01 16:43:58 +0200

    [IMPRV] more debug information's

--- src/main/java/com/capgemini/wsb/fitnesstracker/LoggingAspect.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/LoggingAspect.java
@@ -5,17 +5,56 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Service;
 
+import java.security.Timestamp;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 @Aspect
 @Service
 public class LoggingAspect {
     @Around("within(com.capgemini.wsb.fitnesstracker.*.internal.*ServiceImpl+)\")")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("Run method:: " + joinPoint.getSignature().getName());
-        Object result = joinPoint.proceed();
-        System.out.println("Method stopped: " + joinPoint.getSignature().getName());
-        if (joinPoint.getArgs().length > 0 && result != null) {
-            System.out.println("Result: " + result);
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getName();
+        Object[] args = joinPoint.getArgs();
+
+        LocalDateTime beforeExecution = LocalDateTime.now();
+        StringBuilder logMessage = new StringBuilder();
+        logMessage
+                .append(beforeExecution)
+                .append(" EXECUTING ")
+                .append(className)
+                .append(".")
+                .append(methodName)
+                .append("(");
+
+        for (Object arg : args) {
+            logMessage.append(arg);
         }
+        logMessage.append(")");
+        System.out.println(logMessage);
+        Object result = joinPoint.proceed(args);
+
+        LocalDateTime afterExecution = LocalDateTime.now();
+        StringBuilder logMessageAfterExecution = new StringBuilder();
+        logMessageAfterExecution
+                .append(afterExecution)
+                .append(" FINISHED ")
+                .append(className)
+                .append(".")
+                .append(methodName)
+                .append("(");
+
+        for (Object arg : args) {
+            logMessageAfterExecution.append(arg);
+        }
+
+        logMessageAfterExecution.append(")");
+        logMessageAfterExecution.append(" completed in: ");
+        Duration executionTime = Duration.between(beforeExecution, afterExecution);
+        logMessageAfterExecution.append(executionTime);
+        System.out.println(logMessageAfterExecution);
+
         return result;
     }
 }

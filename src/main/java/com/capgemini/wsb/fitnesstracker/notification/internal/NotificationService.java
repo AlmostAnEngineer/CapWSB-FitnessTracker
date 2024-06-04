6549afcbc832e7aee4a2492b04c312c6afce1c66
AlmostAnEngineer <94685050+AlmostAnEngineer@users.noreply.github.com>
2024-06-04 15:07:06 +0200

    GET TRAININGS STARTED IN LAST WEEK

--- src/main/java/com/capgemini/wsb/fitnesstracker/notification/internal/NotificationService.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/notification/internal/NotificationService.java
@@ -4,6 +4,7 @@ import com.capgemini.wsb.fitnesstracker.mail.api.EmailDto;
 import com.capgemini.wsb.fitnesstracker.mail.api.EmailProvider;
 import com.capgemini.wsb.fitnesstracker.mail.api.EmailSender;
 import com.capgemini.wsb.fitnesstracker.mail.internal.EmailService;
+import com.capgemini.wsb.fitnesstracker.training.api.Training;
 import com.capgemini.wsb.fitnesstracker.training.api.TrainingProvider;
 import com.capgemini.wsb.fitnesstracker.user.api.User;
 import com.capgemini.wsb.fitnesstracker.user.api.UserProvider;
@@ -16,6 +17,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,19 +34,26 @@ public class NotificationService {
     private final UserProvider userProvider;
     private final String reportString="WEEKLY REPORT";
 
-    @Scheduled(cron = "15 48 14 ? * 2") //report scheduled for every sunday at 18:00:00
+    @Scheduled(cron = "15 2 15 ? * 2") //report scheduled for every sunday at 18:00:00
     public void generateReportAndSendMail() {
         System.out.println("Cron scheduling report generation");
         List<User> allUsers = userProvider.findAllUsers();
         LocalDateTime now = LocalDateTime.now();
+        LocalDateTime oneWeekAgo = now.minus(1, ChronoUnit.WEEKS);
         for (User user : allUsers) {
-            final EmailDto emailDto = emailProvider.sendMail(user.getEmail(),
-                    reportString,
-                    trainingProvider.getTrainingsByUserId(user.getId()).stream().filter(
-                            training -> training.getStartTime().toInstant().isAfter(Instant.from(now))
-                    ).collect(Collectors.toList()));
-            emailSender.send(emailDto);
-            System.out.println("sending email");
+            List<Training> recentTrainings = trainingProvider.getTrainingsByUserId(user.getId()).stream()
+                    .filter(training -> toLocalDateTime(training.getStartTime()).isAfter(oneWeekAgo))
+                    .collect(Collectors.toList());
+            if (!recentTrainings.isEmpty()) {
+                final EmailDto emailDto = emailProvider.sendMail(user.getEmail(),
+                        reportString,
+                        recentTrainings);
+                emailSender.send(emailDto);
+                System.out.println("sending email");
+            }
         }
+    }
+    private LocalDateTime toLocalDateTime(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 }
